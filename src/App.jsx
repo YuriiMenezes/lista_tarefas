69def76c443161067ebd3bc777d987c4064aca39
Yuri menezes <yurythc@gmail.com>
2023-02-17 23:48:46 -0400

    second

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import Tasks from './components/Tasks';
+import './App.css'
+import AddTask from './components/AddTask';
+import Button from './components/Button';
+
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+
+    },
+
+    {
+      id: '2',
+      title: 'Ler livros',
+      completed: true,
+
+    },
+
+    {
+      id: '3',
+      title: 'Ler livros',
+      completed: true,
+
+    }
+
+
+  ]);
+
+
+
+  return(
+
+    <div className='container'>
+      <AddTask />
+    
+      <Tasks tasks={tasks}/>
+    </div>
+    
+  )
+
+
+}
+
+export default App;
